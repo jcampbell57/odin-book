73cb4191edc57fa1112dca6163188e22899357da
jcampbell57 <jcampbell57@gmail.com>
2023-11-30 22:39:54 -0800

    add display_name to User

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(email: 'humblebragger@humblebrag.com', password: 'humblebaby')
-User.create(email: 'fitnessgrampacer@test.com', password: 'pacertest')
+User.create(display_name: 'Humble Bragger', email: 'humblebragger@humblebrag.com', password: 'humblebaby')
+User.create(display_name: 'Fitness Grampa', email: 'fitnessgrampacer@test.com', password: 'pacertest')
