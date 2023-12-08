5ebcbe2abf2b23e9df94e4987c43a331b09b405e
jcampbell57 <jcampbell57@gmail.com>
2023-12-08 10:48:43 -0800

    adds helper methods for likes and friendships to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,40 @@ module ApplicationHelper
     comment = Comment.find(notice.notice_id)
     Post.find(comment.post_id)
   end
+
+  # LIKES HELPER
+  # Checks whether a post or comment has already been liked by the
+  # current user returning either true or false
+  def liked?(subject, type)
+    result = false
+    if type == 'post'
+      result = Like.where(user_id: current_user.id,
+                          post_id: subject.id).exists?
+    end
+    if type == 'comment'
+      result = Like.where(user_id: current_user.id,
+                          comment_id: subject.id).exists?
+    end
+    result
+  end
+
+  # FRIENDSHIP HELPERS
+  def friend_request_sent?(user)
+    current_user.sent_friend_requests.exists?(sent_to_id: user.id, status: false)
+  end
+
+  def friend_request_received?(user)
+    current_user.recieved_friend_requests.exists?(sent_by_id: user.id, status: false)
+  end
+
+  # Checks whether a user has had a friend request sent to them by the current user or
+  # if the current user has been sent a friend request by the user returning either true or false
+  def possible_friend?(user)
+    request_sent = current_user.sent_friend_requests.exists?(sent_to_id: user.id)
+    request_received = current_user.recieved_friend_requests.exists?(sent_by_id: user.id)
+
+    return true if request_sent != request_received
+    return true if request_sent == request_received && request_sent == true
+    return false if request_sent == request_received && request_sent == false
+  end
 end
